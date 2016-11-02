4135592d28736673de349c480f0aacb9b03ecdd2
Ye Ding <valency@connect.ust.hk>
2016-11-02 15:55:23 +0800

    v0.11.2

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
     <hr/>
-    <p>(C) 2016 <a href="http://deepera.com" target="_blank">中国电信云公司</a></p>
+    <p>(C) 2016 <a href="javascript:void(0)" target="_blank">中国电信云公司</a></p>
 </footer>
